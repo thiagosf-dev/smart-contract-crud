3cacbad6440c459184284ec050ae635eb4fbeebf
Thiago Sores Figueiredo <thiagosf.dev@gmail.com>
2023-08-29 00:56:33 -0300

    ✨ feat: Innitial commit

---
+++ BookDatabase.sol
@@ -0,0 +1,53 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract BookDatabase {
+    struct Book {
+        string title;
+        uint16 year;
+    }
+
+    mapping(uint32 => Book) public books;
+    address private immutable owner;
+    uint32 private nextId = 0;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        require(owner == msg.sender, "You don't have permission!");
+        _;
+    }
+
+    function compare(
+        string memory strA,
+        string memory strB
+    ) private pure returns (bool) {
+        bytes memory arrA = bytes(strA);
+        bytes memory arrB = bytes(strB);
+        return arrA.length == arrB.length && keccak256(arrA) == keccak256(arrB);
+    }
+
+    function addBook(Book memory newBook) public {
+        nextId++;
+        books[nextId] = newBook;
+    }
+
+    function editBook(uint32 id, Book memory newBook) public {
+        Book memory oldBook = books[id];
+
+        if (
+            !compare(newBook.title, oldBook.title) &&
+            !compare(newBook.title, "")
+        ) books[id].title = newBook.title;
+
+        if (newBook.year != oldBook.year && newBook.year > 0)
+            books[id].year = newBook.year;
+    }
+
+    function removeBook(uint32 id) public restricted {
+        delete books[id];
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Smart Contract CRUD
+
+Project to apply the concepts from Luiz Tools' Web 3.0 course.
+
+## What was learned?
+
+- mapping
+- admin functions
+- function modifier
+- constructor
+- immutable
+- build
+- deploy
+
+### This project can be tested through the link: [smart-contract-crud](<https://testnet.bscscan.com/address/0xB2aD3D148b3C0Cf0ca0E552a7da93683d19D0D43#writeContract>)
